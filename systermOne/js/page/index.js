c6ba8aa5bef504923e0def36d0475bb22357c0b7
YCH <YCH@DESKTOP-00071C3>
2018-07-02 16:42:48 +0800

    搜索修改

--- systermOne/js/page/index.js
+++ systermOne/js/page/index.js
@@ -35,6 +35,11 @@ function keywordSearch(){
                 }
                 $(".keywords_list").html(arr).show();
                 $("#testList").setItemHeight();                        //地图上高亮显示
+                $(".keyList").hover(function(){
+                    $(this).children("img").css("display","block");
+                },function(){
+                    $(this).children("img").css("display","none");
+                });
                 $(".keyList").click(function(){
                     var geo = $(this).next("span").text();
                     var arr = geo.split(",");
@@ -69,8 +74,62 @@ function keywordSearch(){
             }
         });
         $(".keywords_list").css("display","block");
-
     })
+}
+function searchList(){
+    $.ajax({
+        type: "get",
+        url: "js/project.json",
+        dataType: "json",
+        success: function (data) {
+            ProjectListA = new ProjectList(data);
+            var arr = [];
+            for (var i = 0, len = data.length; i < len; i++) {
+                    arr.push("<li class='keyList' id='keyList'>" + data[i].keyword + "<img class='peizhi' data-toggle='modal' data-target='#configModal' src='../../../../testOne/systermOne/img/peizhi.png' alt='配置'></li><span style='display: none;'>"+data[i].geo+"</span>");
+            }
+            $(".keywords_list").html(arr).show();
+            $("#testList").setItemHeight();                        //地图上高亮显示
+            $(".keyList").hover(function(){
+                $(this).children("img").css("display","block");
+            },function(){
+                $(this).children("img").css("display","none");
+            });
+            $(".keyList").click(function(){
+                var geo = $(this).next("span").text();
+                var arr = geo.split(",");
+                for(var i=0;i<arr.length;i++){
+                    var marker = new BMap.Marker(new BMap.Point(arr[0],arr[1]));
+                    map.addOverlay(marker);
+                    // marker.setAnimation(BMAP_ANIMATION_BOUNCE); //跳动的动画
+                }
+                //创建信息窗口
+                var title = $(this).text();
+                var sContent ="";
+                sContent +=    "<div class='infoBox'><h4 class='infoTitle'>"+ title +"</h4>" +
+                    "<p class='process'>工程进度:</p>" +"<p class='mointor'>监测状态:</p>"+
+                    "</div>";
+                var infoWindow = new BMap.InfoWindow(sContent);  // 创建信息窗口对象
+                marker.addEventListener("click", function(){
+                    this.openInfoWindow(infoWindow);
+                    //图片加载完毕重绘infowindow
+                });
+                //右键菜单事件
+                var markerMenu=new BMap.ContextMenu();
+                marker.addEventListener("rightclick",function(){
+                    this.addContextMenu(markerMenu);//给标记添加右键菜单
+                });
+                markerMenu.addItem(new BMap.MenuItem('进入项目',EventMarker.bind(map)));
+                marker.addContextMenu(markerMenu);
+                function EventMarker(map){
+                    console.log(map);
+                    $("#loadBox").load("test.html");
+                }
+            })
+        }
+    });
+    $(".listDown").click(function(){
+        $("#testList").toggle();
+    });
 }
 function outClick(){
     $("#out").click(function(){
@@ -115,6 +174,7 @@ $(function(){
     nowData();                                         //当前日期
     userName();                                        //保存用户名
     outClick();                                         //退出
+    searchList();                                       //搜索列表
     keywordSearch();                                   //关键字搜索
     loadBox();
 });
